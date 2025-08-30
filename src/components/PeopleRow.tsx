17123942b325ad6a2d9d82e8146d3f6ef03ed5dc
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-08-30 20:33:05 +0800

    feat: initialized client socket and chatroom creation

--- src/components/PeopleRow.tsx
+++ src/components/PeopleRow.tsx
@@ -1,5 +1,8 @@
 import type { Props } from "@/types/user";
 import clsx from "clsx";
+import { Button } from "@/components/ui/button";
+import { io } from "socket.io-client";
+import { toast } from "sonner";
 
 const PeopleRow = ({ user }: Props) => {
   const statusClasses = clsx(
@@ -10,20 +13,50 @@ const PeopleRow = ({ user }: Props) => {
     user.status === null && "bg-neutral-500"
   );
 
+  const chatHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const buttonEvent = e.target as HTMLButtonElement;
+    const socket = io(import.meta.env.VITE_SERVER_URL);
+
+    // Get token from LocalStorage to get current user data
+    const token = JSON.parse(localStorage.getItem("token") as string);
+
+    // Tell our io server to create a room for us and the person we
+    // want to chat with
+    socket.emit(
+      "create room",
+      token,
+      buttonEvent.id,
+      (res: { success: boolean }) => {
+        if (res.success) {
+          toast.success("Chatroom created successfully!");
+        }
+      }
+    );
+  };
+
   return (
     <section className="flex justify-between items-center w-full p-4 border-border border rounded-lg">
       <div>
-        <h1 className="text-foreground text-[14px] font-light">
+        <h1 className="text-foreground text-[14px] font-light w-[100px]">
           {user.username}
         </h1>
         {user.bio !== null && <p>{user.bio}</p>}
       </div>
-      <div className="flex items-center gap-2">
+
+      <div className="flex items-center gap-2 self-center mx-auto">
         <div className={statusClasses}></div>
         <p className="font-light text-xs">
           {!user.status ? "Offline" : user.status}
         </p>
       </div>
+
+      <Button
+        className="flex self-end"
+        onClick={chatHandler}
+        id={String(user.id)}
+      >
+        Chat
+      </Button>
     </section>
   );
 };
