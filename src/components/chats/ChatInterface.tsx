7089fccde72348f27e2c6f8c6db18b9cf416aa6c
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-04 21:33:20 +0800

    style: chat scrollbar

--- src/components/chats/ChatInterface.tsx
+++ src/components/chats/ChatInterface.tsx
@@ -62,7 +62,15 @@ const ChatInterface = ({ room }: ChatRoom) => {
 
   return (
     <div className="flex flex-col justify-end w-full max-h-[827px]">
-      <div className="flex flex-col items-end px-10 gap-2 overflow-y-auto">
+      <div
+        className="flex flex-col items-end px-10 gap-2 overflow-y-auto  [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:rounded-full
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:rounded-full
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 mt-2"
+      >
         {/* Render backend chat history here */}
         {chatMessages?.data.map((message: MessageInterface) => {
           return (
