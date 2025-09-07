f6e7e27bc2c0744536b4effabd511797965d6375
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-07 14:11:25 +0800

    feat; people searching and filtering

--- src/pages/authenticated/People.tsx
+++ src/pages/authenticated/People.tsx
@@ -8,23 +8,28 @@ import { SelectPeople } from "@/components/people/SelectFilterPeople";
 
 const People = () => {
   const [statusFilter, setStatusFilter] = useState<null | string>(null);
+  const [search, setSearch] = useState<null | string>(null);
 
   // Upon mounting component, query all people
   const { data, refetch } = useQuery({
-    queryKey: ["users", statusFilter],
+    queryKey: ["users", statusFilter, search],
     queryFn: async () => {
       const token = JSON.parse(localStorage.getItem("token") as string);
-      const res = await fetch(
-        `${import.meta.env.VITE_SERVER_URL}/api/users${
-          statusFilter !== null ? `?filter=${statusFilter}` : ""
-        }`,
-        {
-          headers: {
-            authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      const users = res.json();
+      let url = `${import.meta.env.VITE_SERVER_URL}/api/users`;
+      if (statusFilter && search) {
+        url = `${url}?filter=${statusFilter}&search=${search}`;
+      } else if (statusFilter) {
+        url = `${url}?filter=${statusFilter}`;
+      } else if (search) {
+        url = `${url}?search=${search}`;
+      }
+      console.log(url);
+      const res = await fetch(url, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      const users = await res.json();
       return users;
     },
   });
@@ -45,6 +50,7 @@ const People = () => {
           type="search"
           className="w-[50%] grow-1"
           placeholder="Search someone..."
+          onChange={(e) => setSearch(e.target.value)}
         ></Input>
         <SelectPeople setStatusFilter={setStatusFilter}></SelectPeople>
       </div>
