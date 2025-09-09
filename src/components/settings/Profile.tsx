ca43c601c6f33207218ac9adf1cbc86be746eb89
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-09 16:22:15 +0800

    feat: avatar file upload functionality in profile page

--- src/components/settings/Profile.tsx
+++ src/components/settings/Profile.tsx
@@ -3,6 +3,9 @@ import { Label } from "@radix-ui/react-label";
 import useUser from "@/hooks/useUser";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState, useRef } from "react";
+import clsx from "clsx";
 
 const ProfileInputRow = ({
   label,
@@ -27,6 +30,21 @@ const ProfileInputRow = ({
 
 const Profile = () => {
   const { user } = useUser();
+  const [hover, setHover] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>(null);
+  const editAvatarBtn = useRef<SVGSVGElement | null>(null);
+  const avatarInputRef = useRef<HTMLInputElement | null>(null);
+
+  const avatarUploadHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    avatarInputRef?.current?.click();
+  };
+
+  const uploadAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
 
   return (
     <form className="flex justify-around w-full gap-4 p-4 ">
@@ -45,12 +63,41 @@ const Profile = () => {
       </div>
 
       <div className="flex flex-col items-center gap-2">
-        <img
-          src={`${import.meta.env.VITE_R2_PUBLIC_URL}/${user?.data[0].avatar}`}
-          alt="user avatar"
-          className="w-[150px] h-auto rounded-full object-contain"
-        />
+        <button
+          className="w-[150px] h-[150px] rounded-full relative hover:bg-neutral-500 hover:opacity-70"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          onClick={avatarUploadHandler}
+        >
+          <img
+            src={`${
+              file
+                ? URL.createObjectURL(file)
+                : `${import.meta.env.VITE_R2_PUBLIC_URL}/${
+                    user?.data[0].avatar
+                  }`
+            }`}
+            alt="user avatar"
+            className="w-[150px] h-[150px] rounded-full object-cover"
+          />
+          <Pencil
+            className={clsx(
+              "absolute top-[40%] left-[40%] size-[32px]",
+              hover ? "!opacity-100 visible stroke-white" : "invisible"
+            )}
+            ref={editAvatarBtn}
+          ></Pencil>
+        </button>
         <p className="font-light text-sm">Profile avatar</p>
+        <input
+          type="file"
+          name="avatar"
+          id="avatar"
+          className="invisible"
+          ref={avatarInputRef}
+          onChange={uploadAvatar}
+          accept="image/*"
+        />
       </div>
     </form>
   );
