fbc5a95abe25ff9ff698011f95f0302363f8f437
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-19 13:55:27 +0800

    refactor: navigation to use shadcn nav

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,13 @@
 import type { ReactProps } from "@/types/reactNode";
 import { ModeToggle } from "@/components/ModeToggle";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { socket } from "../socket";
 import { useEffect } from "react";
 import protectedLoader from "@/lib/protectedLoader";
 import useUser from "@/hooks/useUser";
 import { HeaderDropdown } from "@/layouts/HeaderDropdown";
 import ChangeStatus from "@/layouts/ChangeStatus";
+import Navigation from "@/layouts/Navigation";
 
 const Layout = ({ children }: ReactProps) => {
   const navigate = useNavigate();
@@ -40,28 +41,7 @@ const Layout = ({ children }: ReactProps) => {
   return (
     <main className="grid grid-cols-[1fr_1024px_1fr] grid-rows-[62px_1fr_28px] p-4 gap-2 h-full max-h-full">
       <header className="flex justify-between items-center h-fit p-3 col-start-2 col-end-3 row-start-1 row-end-2 border-border border bg-card rounded-sm">
-        <nav className="flex list-none gap-4">
-          <NavLink
-            to="/chat"
-            className={({ isActive }) =>
-              isActive
-                ? "underline underline-offset-4"
-                : "no-underline hover:underline hover:underline-offset-4"
-            }
-          >
-            Chats
-          </NavLink>
-          <NavLink
-            to="/people"
-            className={({ isActive }) =>
-              isActive
-                ? "underline underline-offset-4"
-                : "no-underline hover:underline hover:underline-offset-4"
-            }
-          >
-            People
-          </NavLink>
-        </nav>
+        <Navigation></Navigation>
         <div className="flex items-center gap-3 ">
           <ChangeStatus
             user={user?.data[0]}
---
+++ src/layouts/Navigation.tsx
@@ -0,0 +1,23 @@
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuLink className="cursor-pointer select-none">
+          <Link to="/chat">Chats</Link>
+        </NavigationMenuLink>
+        <NavigationMenuLink className="cursor-pointer select-none">
+          <Link to="/people">People</Link>
+        </NavigationMenuLink>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default Navigation;
