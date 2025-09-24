ffc98f4c0a87a0d48c2b82c564d157c7001736ad
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-24 10:43:53 +0800

    fix: remove unnecessary prop

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -24,10 +24,9 @@ export function ThemeProvider({
   children,
   defaultTheme = "system",
   storageKey = "vite-ui-theme",
-  ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
   );
 
   useEffect(() => {
